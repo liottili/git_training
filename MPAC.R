23374410369093633645eed72b3083cb32f8a637
Linda Liotti <linda.liotti@mpac.ca>
2016-09-15 13:28:23 -0400

    I added a comment

--- MPAC.R
+++ MPAC.R
@@ -1,17 +1,19 @@
-# Read in the (fake) 2020.csv file
+#Read in the (fake) 2020.csv file
 # Read in a text file and make sure that the first column is character
 # http://stackoverflow.com/questions/2805357/specifying-colclasses-in-the-read-csv
 column_classes=c("character","numeric","character")
 data<-read.csv("validate-vars.txt", colClasses=column_classes)
 
 
-# Frequency
-# Compare Means
-# Cross Tabs
-# Validate Variables
+#frequency counts
+#compare means
+#cross tabs
+#validate variables
+#summing by ID
 
+# only print rows that have a valid variable in "Var": A,B,C,D (not E)
+# http://stackoverflow.com/questions/1686569/filtering-a-data-frame-in-r
+validdata<-data[data$Var %in% c("A","B","C","D"),]
 
-# Summing by ID
-# sum the "TotalCost" column for each "ID" column
-# http://stackoverflow.com/questions/10180132/consolidate-duplicate-rows
-aggdata<-aggregate(data["TotalCost"],by=data["ID"],sum)
+#hello
+this is just a test
